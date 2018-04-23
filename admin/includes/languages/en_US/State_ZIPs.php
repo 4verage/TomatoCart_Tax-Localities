bfee8c75dada6be0cef11a9c0ec664cb04ae0059
Jonathan E. Styles <35280539+4verage@users.noreply.github.com>
2018-04-22 23:05:35 -0400

    Add files via upload
    All text used in the module when derived from the code.

---
+++ admin/includes/languages/en_US/State_ZIPs.php
@@ -0,0 +1,61 @@
+# $Id: tax_classes.php $
+# TomatoCart Open Source Shopping Cart Solutions
+# http://www.tomatocart.com
+#
+# Copyright (c) 2009 Wuxi Elootec Technology Co., Ltd; Copyright (c) 2007 osCommerce
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License v2 (1991)
+# as published by the Free Software Foundation.
+
+heading_title = State ZIPs
+
+table_heading_State_ZIPs = State
+table_heading_states_country = Country
+table_heading_base_tax = Base Tax
+table_heading_total_entries = # Entries
+table_heading_action = Action
+
+table_heading_ZIP_rate_priority = Locality Name
+table_heading_ZIP_rate_zone = ZIP Code
+table_heading_ZIP_rate = Tax Rate
+table_heading_total_rate = Total Taxes
+
+action_heading_new_state_zip = Select Existing Country and State
+action_heading_batch_delete_State_ZIPs = Batch Delete Tax Classes
+
+action_heading_new_ZIP_rate = New ZIP Rate
+action_heading_batch_delete_ZIP_rates = Batch Delete Tax Rates
+
+section_ZIP_rates = Tax Rates by ZIP Code
+
+field_country = Country:
+field_zone = Zone:
+field_base_tax = State Base Sales Tax Rate:
+
+field_ZIP_rate_zone_group = State:
+field_ZIP = ZIP (5 digits):
+field_ZIP_rate_description = City / Locality:
+field_ZIP_rate = Tax Rate (%):
+
+total_entries = %s Entries
+all_countries = All Countries
+all_zones = All Zones
+
+introduction_new_State_ZIP = Please fill in the following information for the new tax class.
+
+introduction_edit_State_ZIP = Please make the necessary changes to the following tax class.
+
+introduction_delete_State_ZIP = Please verify the removal of this tax class and its tax rate entries.
+delete_warning_State_ZIP_in_use = Warning: This tax class cannot be deleted as it is currently assigned to %s products.
+
+introduction_batch_delete_State_ZIPs = Please verify the removal of the following tax classes and their tax rate entries.
+batch_delete_warning_State_ZIP_in_use = Warning: The following tax classes cannot be deleted as they are currently assigned to products.
+
+introduction_new_ZIP_rate = Please fill in the following information for the new tax rate entry:
+
+introduction_edit_ZIP_rate = Please make the necessary changes to the following tax rate entry:
+
+introduction_delete_ZIP_rate = Please verify the removal of this tax rate entry:
+
+introduction_batch_delete_ZIP_rates = Please verify the removal of the following tax rate entries:
